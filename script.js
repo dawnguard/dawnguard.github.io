6fb8da1d0aee6a27bdb0b821ea0bd22ad534cc5e
Karan Bhullar <elusivefoxy@gmail.com>
2016-09-27 06:00:26 +0530

    add scrollspy

---
+++ script.js
@@ -0,0 +1,18 @@
+//// Scrollspy Select
+//$('body').scrollspy({target: '#top-nav'});
+////$('body').scrollspy({target: '#projecty'});
+////$('body').scrollspy({target: '#contacty'});
+//// ================
+//
+//// Change nav item class to selected
+//$('#top-nav').on('activate.bs.scrollspy', function(){
+////  $('this').addClass('selected');
+//  console.log("Scrollspy");
+//});
+//$(".scroll-area").scrollspy({target: "#myNavbar"}) $(".scroll-area").scrollspy({target: "#myNavbar"}) 
+
+//$('#thisthing').scrollspy({ target: '#skilly' })
+
+//$('[data-spy="scroll"]').each(function () {
+//  alert();
+//})
